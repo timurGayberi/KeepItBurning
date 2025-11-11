0e086db7a6a1aa91f310accceef99657911fd82b
Kubilay Teker <yusuf-kubi@hotmail.com>
2025-11-11 16:46:00 +0100

    small fixes

---
+++ KeepItBurning/Assets/Scripts/Visitors/VisitorsManager.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class VisitorsManager : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> visitorPrefabs = new List<GameObject>();
+    [SerializeField] private int maxVisitors;
+    [SerializeField] private float CountToSpawVisitors;
+    [SerializeField] private float TimeToSpawVisitors;
+    [SerializeField] private List<Transform> spawnPoints = new List<Transform>();
+
+    private List<GameObject> activeVisitors = new List<GameObject>();
+    private List<Transform> availableSpawnPoints = new List<Transform>();
+    private Dictionary<GameObject, Transform> visitorSpawnMap = new Dictionary<GameObject, Transform>();
+
+    void Start()
+    {
+        availableSpawnPoints = new List<Transform>(spawnPoints);
+        SpawnVisitor();
+    }
+
+    void Update()
+    {
+        CountToSpawVisitors += Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.C))
+            SpawnVisitor();
+        if (Input.GetKeyDown(KeyCode.V) && activeVisitors.Count > 0)
+            RemoveVisitor(activeVisitors[0]);
+        if (CountToSpawVisitors > TimeToSpawVisitors)
+        {
+            CountToSpawVisitors = 0f;
+            SpawnVisitor();
+        }
+
+    }
+
+    public void SpawnVisitor()
+    {
+        if (activeVisitors.Count >= maxVisitors)
+        {
+            return;
+        }
+
+        if (availableSpawnPoints.Count == 0)
+        {
+            return;
+        }
+
+        GameObject chosenPrefab = visitorPrefabs[Random.Range(0, visitorPrefabs.Count)];
+        int randomIndex = Random.Range(0, availableSpawnPoints.Count);
+        Transform chosenPoint = availableSpawnPoints[randomIndex];
+
+        availableSpawnPoints.RemoveAt(randomIndex);
+
+        GameObject newVisitor = Instantiate(chosenPrefab, chosenPoint.position, chosenPoint.rotation);
+        activeVisitors.Add(newVisitor);
+
+        visitorSpawnMap[newVisitor] = chosenPoint;
+    }
+
+    public void RemoveVisitor(GameObject visitor)
+    {
+        if (!activeVisitors.Contains(visitor))
+            return;
+
+        activeVisitors.Remove(visitor);
+
+        if (visitorSpawnMap.ContainsKey(visitor))
+        {
+            availableSpawnPoints.Add(visitorSpawnMap[visitor]);
+            visitorSpawnMap.Remove(visitor);
+        }
+
+        Destroy(visitor);
+    }
+
+}
