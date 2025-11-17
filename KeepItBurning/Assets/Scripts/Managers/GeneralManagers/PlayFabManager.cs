2aea2dcd0f976d19e30dc6116176700014113d0e
timurGayberi <mehmet.gayberi@edu.futuregames.se>
2025-11-17 14:42:49 +0100

    Login success

--- KeepItBurning/Assets/Scripts/Managers/GeneralManagers/PlayFabManager.cs
+++ KeepItBurning/Assets/Scripts/Managers/GeneralManagers/PlayFabManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using PlayFab;
+using PlayFab.ClientModels;
 
 namespace Managers.GeneralManagers
 {
@@ -7,10 +8,49 @@ namespace Managers.GeneralManagers
     {
         
         #region References
-        
-        private const string PlayFabTitleID = "1ECE61";
-        
+
+        private const string    PlayFabTitleID =        "1ECE61",
+                                PlayerScore =           "Score",
+                                PlayerPlatformScore =   "Game Score",
+                                PlayerNickName =        "Nickname";
+
         #endregion
         
+        private bool                                    _loginSuccess;
+
+        private void Start()
+        {
+            PlayFabSettings.staticSettings.TitleId = PlayFabTitleID;
+            
+            Login();
+        }
+        
+        #region Login setup
+
+        private void Login()
+        {
+            var request = new LoginWithCustomIDRequest()
+            {
+                CustomId = SystemInfo.deviceUniqueIdentifier,
+                CreateAccount = true
+            };
+
+            PlayFabClientAPI.LoginWithCustomID(request, OnSuccess, OnError);
+        }
+
+        private void OnSuccess(LoginResult result)
+        {
+            Debug.Log("Login Success");
+            _loginSuccess = true;
+        }
+
+        private void OnError(PlayFabError error)
+        {
+            Debug.LogError("PlayFab login Failed!" + error.GenerateErrorReport());
+            _loginSuccess = false;
+        }
+        
+        #endregion
+
     }
 }
