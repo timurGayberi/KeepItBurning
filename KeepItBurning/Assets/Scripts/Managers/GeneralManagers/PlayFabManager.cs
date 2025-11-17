a0cf2a939b1109f4b79fd4f3a7d0f3ccbb08ba08
timurGayberi <mehmet.gayberi@edu.futuregames.se>
2025-11-17 13:58:40 +0100

    playFab init

---
+++ KeepItBurning/Assets/Scripts/Managers/GeneralManagers/PlayFabManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using PlayFab;
+
+namespace Managers.GeneralManagers
+{
+    public class PlayFabManager : MonoBehaviour
+    {
+        
+        #region References
+        
+        private const string PlayFabTitleID = "1ECE61";
+        
+        #endregion
+        
+    }
+}
